6adb19ba31718abbf90d13d9a3c295afbd4e21b3
amirutkirov99 <92102608+amirutkirov99@users.noreply.github.com>
2024-07-04 00:26:55 +0000

    BIG FIKS

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from aiogram.enums import ParseMode
 import asyncio
 import requests
 import os
+import shutil
 import re
 from background import keep_alive
 
@@ -19,17 +20,32 @@ def escape_markdown(text: str) -> str:
     return ''.join(f'\\{char}' if char in escape_chars else char for char in text)
 
 
+# url = "https://tiktok-nowatermark-full-services.p.rapidapi.com/api"
+
+# payload = { "url": "https://www.tiktok.com/@vladislav.mmm/video/7383582429346336007" }
+# headers = {
+# 	"x-rapidapi-key": "45f4e95f74mshb7231980c6f3821p13ff84jsnac7950a93cb8",
+# 	"x-rapidapi-host": "tiktok-nowatermark-full-services.p.rapidapi.com",
+# 	"Content-Type": "application/json"
+# }
+
+# response = requests.post(url, json=payload, headers=headers)
+
+# print(response.json())
+
+
 def download_tt(url_tiktok):
-    url = "https://tiktok-video-no-watermark2.p.rapidapi.com/"
-    payload = f"-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"url\"\r\n\r\n{url_tiktok}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"hd\"\r\n\r\n1\r\n-----011000010111000001101001--\r\n\r\n"
-    headers = {
-        "x-rapidapi-key": "45f4e95f74mshb7231980c6f3821p13ff84jsnac7950a93cb8",
-        "x-rapidapi-host": "tiktok-video-no-watermark2.p.rapidapi.com",
-        "Content-Type": "multipart/form-data; boundary=---011000010111000001101001"
+    url = "https://www.tikwm.com/api/"
+    payload = {
+        'url': f"{url_tiktok}",
+        'count': 12,
+        'cursor': 0,
+        'web': 1,
+        'hd': 1
     }
 
-    response = requests.post(url, data=payload, headers=headers)
-    response_json = response.json()
+    response = requests.get(url, params=payload)
+    response_json = response.json()['data']
     return response_json
 
 # title = response_json['data']['title']
@@ -40,11 +56,12 @@ def download_tt(url_tiktok):
 
 # Введите сюда токен вашего бота
 BOT_TOKEN = '6846762920:AAFYPWrc16abK9CK-oHknshcn22tiZAqkpE'
+# BOT_TOKEN = '6308423351:AAEdjuR5wMid8ovw8QOZn6jEGC4gz9nqm44'  # kino poisk
 # Идентификатор чата
 # CHAT_ID = '5527705092'
 # URL видео
 # video_url = "tiktok_videos/video.mp4"
-video_url = "D:\Desktop\cru\\tiktok_videos\\video.mp4"
+# video_url = "D:\Desktop\cru\\tiktok_videos\\video.mp4"
 
 
 logging.basicConfig(level=logging.INFO)
@@ -56,6 +73,7 @@ dp = Dispatcher()
 
 
 async def download(url: str, path: str):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     # print("Загрузка")
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
@@ -71,32 +89,51 @@ async def download(url: str, path: str):
 def delete_file(path: str):
     try:
         if os.path.exists(path):
-            os.remove(path)
+            shutil.rmtree(path)
         else:
             pass
     except Exception as e:
-        print(f"Не удалось удалить файл {path}: {e}")
+        print(f"Не удалось удалить папку {path}: {e}")
+
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.reply(f"Добро пожаловать!\n\nВы можете скинуть мне ссылку на пост TikTok откуда нужно выгрузить видео и текст — через пару секунд этот видос будет у вас!")
 
+
 @dp.message(F.text.contains("tiktok.com"))
 async def handle_tiktok_link(message: Message):
-    dev_link_escaped = escape_markdown("Скачано с помощью @tiktok_downloadcr_bot\nРазработчик бота: @ameerchik6")
+    dev_link_escaped = escape_markdown(
+        "Скачано с помощью @tiktok_downloadcr_bot\nРазработчик бота: @ameerchik6")
     dev_link = "Скачано с помощью @tiktok_downloadcr_bot\nРазработчик бота: @ameerchik6"
     try:
-        music_path = "audio.mp3"
-        video_path = "video.mp4"
+        user_id = message.from_user.id
+        music_path = f"id{user_id}/audio.mp3"
+        video_path = f"id{user_id}/video.mp4"
+
         # url_tt = message.text.replace("/send_video ", "")
         url_tt = message.text
+
         json_tt = download_tt(url_tt)
-        title = json_tt['data']['title']
-        hdplay = json_tt['data']['play']
-        music_title = json_tt['data']['music_info']['title']
-        music_author = json_tt['data']['music_info']['author']
-        music_url = json_tt['data']['music_info']['play']
-        video_url = hdplay
+        print(json_tt)
+        # print(json_tt)
+        # json_tt = download_tt(url_tt)
+        # if 'images' in json_tt:
+        #     images_urls = json_tt['images']
+        #     # print(len(images_urls))
+        #     # for i in range(len(images_urls)):
+        #     #     images_path = f"id{user_id}/{i}.jpg"
+        #     #     print(images_urls[i])
+        #     #     download(images_urls[i], images_path)
+        #     print(images_urls)
+        title = json_tt['title']
+        print("title: ", title)
+        hdplay = json_tt['play']
+        music_title = json_tt['music_info']['title']
+        music_author = json_tt['music_info']['author']
+        music_url = "https://www.tikwm.com/" + \
+            json_tt['music']
+        video_url = "https://www.tikwm.com/" + hdplay
         await download(video_url, video_path)
         await download(music_url, music_path)
         video_input = FSInputFile(video_path)
@@ -104,29 +141,32 @@ async def handle_tiktok_link(message: Message):
 
         # Отправляем видео
         # print("Отправляю")
-        if os.path.exists(video_path):
-            # Получаем размер файла в байтах
-            file_size = os.path.getsize(video_path)
-            # Конвертируем размер в килобайты
-            file_size_kb = file_size / 1024
-            threshold_kb = 50 * 1024
-            # Проверяем размер файла по пороговому значению
-            if file_size_kb < threshold_kb:
-                if title == "":
-                    await bot.send_video(chat_id=message.from_user.id, video=video_input, caption=f"{dev_link}")
+        if True:
+            if os.path.exists(video_path):
+                # Получаем размер файла в байтах
+                file_size = os.path.getsize(video_path)
+                # Конвертируем размер в килобайты
+                file_size_kb = file_size / 1024
+                threshold_kb = 50 * 1024
+                # Проверяем размер файла по пороговому значению
+                if file_size_kb < threshold_kb:
+                    if title == "":
+                        await bot.send_video(chat_id=message.from_user.id, video=video_input, caption=f"{dev_link}")
+                    else:
+                        await bot.send_video(chat_id=message.from_user.id, video=video_input, caption=f"```Описание\n{escape_markdown(title)}```\n\n{dev_link_escaped}", parse_mode=ParseMode.MARKDOWN_V2)
                 else:
-                    await bot.send_video(chat_id=message.from_user.id, video=video_input, caption=f"```Описание\n{escape_markdown(title)}```\n\n{dev_link_escaped}", parse_mode=ParseMode.MARKDOWN_V2)
-            else:
-                text = escape_markdown(
-                    "Видео, которое вы хотите сохранить весит более 50 Мб. Поэтому данное видео доступно для скачивания только по ссылке ниже!")
-                await bot.send_message(chat_id=message.from_user.id, text=f"{text}\n\n[Скачать видео]({hdplay})", parse_mode=ParseMode.MARKDOWN_V2)
-        await bot.send_audio(chat_id=message.from_user.id, audio=music_input, title=music_title, performer=music_author)
+                    text = escape_markdown(
+                        "Видео, которое вы хотите сохранить весит более 50 Мб. Поэтому данное видео доступно для скачивания только по ссылке ниже!")
+                    await bot.send_message(chat_id=message.from_user.id, text=f"{text}\n\n[Скачать видео]({video_url})", parse_mode=ParseMode.MARKDOWN_V2)
+                await bot.send_audio(chat_id=message.from_user.id, audio=music_input, title=music_title, performer=music_author)
+        # elif images_urls:
+        #     print("Есть!")
 
     except Exception as e:
         await message.reply(f"Не удалось отправить видео или введенная ссылка неправильная! {e}")
     finally:
-        delete_file(music_path)
-        delete_file(video_path)
+        pass
+        delete_file(f"id{user_id}")
 
 
 async def main():
